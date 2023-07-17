1191567161a326d2dffb31c881ddbd9f5d524f6e
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-07-17 16:14:31 +0200

    trying to implement trait

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
 <?php
+        trait Discount{
+            public $discountValue;
+
+            public function getDiscount(){
+                return "-$this->discountValue %";
+            }
+        }
+
     class Product{
+        use Discount;
+
         public $name;
         public $price;
 
-        function __construct($_name, $_price){
+        function __construct($_name, $_price, $_discountValue){
             $this->name = $_name;
             $this->price = $_price;
+            $this->discountvalue = $_discountValue;
         }
     }
 
     class Target extends Product{
         public $animalTarget;
 
-        function __construct($_name, $_price, $_animalTarget){
-            parent::__construct($_name, $_price);
+        function __construct($_name, $_price, $_discountValue, $_animalTarget){
+            parent::__construct($_name, $_price, $_discountValue);
             $this->animalTarget = $_animalTarget;
         }
     }
@@ -21,22 +32,22 @@
     class Item extends Target{
         public $productType;
 
-        function __construct($_name, $_price, $_animalTarget, $_productType){
-            parent::__construct($_name, $_price, $_animalTarget);
+        function __construct($_name, $_price, $_discountValue, $_animalTarget, $_productType){
+            parent::__construct($_name, $_price, $_discountValue, $_animalTarget);
             $this->productType = $_productType;
         }
     }
 
-    $catPerch = new Item('Perch', 20, 'Cat', 'Perch for cats, currently listening to Velcro Fly by ZZ Top.');
-    $catBall = new Item('Ball', 5, 'Cat', 'Ball for cats, currently listening to I Could Cry by Buddy Guy.');
-    $catRope = new Item('Rope', 3, 'Cat', 'Rope for cats, currently listening to Sweet Home Chicago by The Blues Brothers.');
-    $catFakeFerret = new Item('Fake Ferret', 15, 'Cat', 'Fake Ferret for cats, currently listening to Learn To Fly by Foo Fighters.');
-    $catnip = new Item('Catnip', 6, 'Cat', 'Catnip, currently listening to Never Meant by American Football.');
-    $dogAntler = new Item('Deer Antler', 9, 'Dog', 'Deer Antles for dogs, currently listening to Redneck by Lamb Of God.');
-    $dogKennel = new Item('Kennel', 65, 'Dog', 'Kennel for dogs, currently listening to G.O.A.T. by Polyphia.');
-    $dogHarness = new Item('Harness', 20, 'Dog', 'Harness for dogs, currently listening to Toxic by Britney Spears.');
-    $dogTreats = new Item('Treats', 4, 'Dog', 'Treats for dogs, currently listening to Attention by Doja Cat.');
-    $dogBowl = new Item('Steel Bowl', 7, 'Dog', 'Steel Bowl for dogs, currently listening to Nocturne Op.9 No.2 by Chopin.');
+    $catPerch = new Item('Perch', 20.00, 20, 'Cat', 'Perch for cats, currently listening to Velcro Fly by ZZ Top.');
+    $catBall = new Item('Ball', 5.00, 70, 'Cat', 'Ball for cats, currently listening to I Could Cry by Buddy Guy.');
+    $catRope = new Item('Rope', 3.00, 0, 'Cat', 'Rope for cats, currently listening to Sweet Home Chicago by The Blues Brothers.');
+    $catFakeFerret = new Item('Fake Ferret', 15.00, 10, 'Cat', 'Fake Ferret for cats, currently listening to Learn To Fly by Foo Fighters.');
+    $catnip = new Item('Catnip', 6.00, 60, 'Cat', 'Catnip, currently listening to Never Meant by American Football.');
+    $dogAntler = new Item('Deer Antler', 9.00, 20, 'Dog', 'Deer Antles for dogs, currently listening to Redneck by Lamb Of God.');
+    $dogKennel = new Item('Kennel', 65.00, 40, 'Dog', 'Kennel for dogs, currently listening to G.O.A.T. by Polyphia.');
+    $dogHarness = new Item('Harness', 20.00, 0, 'Dog', 'Harness for dogs, currently listening to Toxic by Britney Spears.');
+    $dogTreats = new Item('Treats', 4.00, 0, 'Dog', 'Treats for dogs, currently listening to Attention by Doja Cat.');
+    $dogBowl = new Item('Steel Bowl', 7.00, 10, 'Dog', 'Steel Bowl for dogs, currently listening to Nocturne Op.9 No.2 by Chopin.');
 
     $products = [$catPerch, $catBall, $catRope, $catFakeFerret, $catnip, $dogAntler, $dogKennel, $dogHarness, $dogTreats, $dogBowl];
 ?>
@@ -67,13 +78,27 @@
                                 ?>
                             </h2>
 
-                            <h3>
-                                <?php
-                                    echo $item->price;
-                                ?> 
+                            <?php if($item->discountValue === 0){ ?>
+                                <h3>
+                                    <?php
+                                        echo $item->price;
+                                    ?> 
 
-                                &euro;
-                            </h3>
+                                    &euro;
+                                </h3>
+                            <?php } else{ ?>
+                                <h3 class="text-danger">
+                                    <?php 
+                                        echo $item->getDiscount();
+                                    ?>
+                                </h3>
+
+                                <h3>
+                                    <?php
+                                        echo ($item->price * $item->discountValue) / 100;
+                                    ?>
+                                </h3>
+                            <?php } ?>
 
                             <h5>
                                 <?php
